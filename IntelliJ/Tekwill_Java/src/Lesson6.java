2195f1663b685cc557857f7bb1d6e4ec4dfcb8aa
Verginia Stolear <Verginia.Stolear@gmail.com>
2022-07-22 18:00:18 +0300

    Lesson6 task added

---
+++ IntelliJ/Tekwill_Java/src/Lesson6.java
@@ -0,0 +1,27 @@
+
+/*
+Scrieti o clasa cu metoda main in ea. Definiti o variabila de tip numeric intreg.
+Daca valoare variabilei este un numar intreg si mai mare ca 10, atunci printati mesajul "Numarul este par."
+Daca numarul este impar si mai mic de 15, atunci printati un mesaj "Numarul este impar".
+ */
+
+import java.util.Scanner;
+
+public class Lesson6 {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0 && num > 10)
+            System.out.println(" Numarul este par");
+        else  if (num % 2 == 1 && num < 15)
+            System.out.println(" Numarul este impar");
+        else {
+            System.out.println(" Mai incearca o data");
+        }
+    }
+}
